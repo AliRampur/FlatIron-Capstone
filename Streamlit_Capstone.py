c050857c48dc37bbb8ff58a8b5bef100761ff02d
AliRampur <108304791+AliRampur@users.noreply.github.com>
2022-10-05 12:58:35 -0600

    Update Streamlit_Capstone.py

--- Streamlit_Capstone.py
+++ Streamlit_Capstone.py
@@ -7,7 +7,7 @@ from locale import currency
 from operator import index
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import pickle
 import numpy as np
 # import seaborn as sns
